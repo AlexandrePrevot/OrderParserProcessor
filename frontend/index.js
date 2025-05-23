17abec891f028902187ae204a9eb7fd313dc7331
Alexandre Prevot <alexandre.prevot91@gmail.com>
2025-05-23 23:21:27 +0200

    introduce JavaScript for FE

---
+++ frontend/index.js
@@ -0,0 +1,14 @@
+// comment
+console.log('hello world!');
+
+const salut = "heeeee";
+
+let person = {
+    name: 'alexandre',
+    age : 30
+};
+
+console.log(salut);
+console.log(typeof(salut));
+console.log(person)
+console.log(typeof(person))
