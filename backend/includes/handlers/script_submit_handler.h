9527510f3cef428a0809e797446de8dea70b11ed
Alexandre Prevot <alexandre.prevot91@gmail.com>
2025-06-16 22:00:15 +0200

    grpc cpp done

--- backend/includes/handlers/script_submit_handler.h
+++ backend/includes/handlers/script_submit_handler.h
@@ -7,20 +7,11 @@
 
 class ScriptSubmitHandler {
 public:
-  /*
-      after generating protofiles
-
-      1 - instantiate gRPC ScriptSubmitService when building
-
-      2 -
-      make a settable function pointer to call when receiving message
-      make a very simple ScriptSubmitProcessor class in /processor
-
-      that will instantiate the ScriptSubmitProcessor that will
-      return a SynchronousReply
-  */
-
   ScriptSubmitHandler();
+
+  ScriptSubmitService* GetScriptSubmitServicePtr() {
+    return service_.get();
+  }
 private:
 
   std::unique_ptr<ScriptSubmitService> BuildService();
--- backend/main.cc
+++ backend/main.cc
@@ -1,12 +1,23 @@
 #include <iostream>
 
-#include "includes/handlers/script_submit_handler.h"
-#include "services/script_submit_service.h"
 #include "handlers/script_submit_handler.h"
 #include "processors/script_submit_processor.h"
+#include "services/script_submit_service.h"
+
+void RunServer() {
+  const std::string server_address("0.0.0.0:50051");
+  ScriptSubmitHandler script_submit_handler;
+
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(script_submit_handler.GetScriptSubmitServicePtr());
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  server->Wait();
+}
 
 int main() {
-    std::cout << "hellow world!" << std::endl;
-    ScriptSubmitHandler script_submit_handler;
-    return 0;
+  std::cout << "hello world!" << std::endl;
+  RunServer();
+  return 0;
 }
