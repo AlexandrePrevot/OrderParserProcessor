0ceedcbab68873c278641b3d085692b05a56e9c7
Alexandre Prevot <alexandre.prevot91@gmail.com>
2025-11-27 11:35:28 +0100

    antlr compiled and can run, need to add framework of test now

--- backend/src/processors/script_submit_processor.cc
+++ backend/src/processors/script_submit_processor.cc
@@ -1,11 +1,25 @@
 #include "processors/script_submit_processor.h"
 
 #include <iostream>
+#include <string>
+
+#include "antlr/FiScriptLexer.h"
+#include "antlr/FiScriptParser.h"
 
 bool ScriptSubmitProcessor::Process(
     const internal::ScriptSubmitRequest &script_submit_request) {
 
+
+  std::string salut = "REACT(180)";
+  antlr4::ANTLRInputStream input(salut);
+  FiScriptLexer lexer(&input);
+  antlr4::CommonTokenStream tokens(&lexer);
+  FiScriptParser parser(&tokens);
+
+  FiScriptParser::FirstContext* tree = parser.first();
+
   std::cout << "processing script submission in the name of "
             << script_submit_request.user() << std::endl;
+
   return true;
 }
